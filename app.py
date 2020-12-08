44a353999931af4011661d0ae4bf7ff18aae9462
Heinrich Bangui <heinrichbangui@gmail.com>
2020-12-08 23:57:55 +0800

    HCM-7: Created Authentication Module

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from db import db
 from flask import Flask
 from flask_restful import Api
+from flask_jwt import JWT
+from datetime import timedelta
+
+from security import authenticate, identity
 
 from resources.joborders import JobOrder, JobOrderList, UUID
+from resources.user import UserRegister
 
 app = Flask(__name__)
+
+app.secret_key = "SECRET"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-api = Api(app)
+# app.config['JWT_AUTH_URL_RULE'] = '/login' # IF we want to change authentication endpooint
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(hours=8) # Set to 8 hours, change the duration of the token
 
+api = Api(app)
+jwt = JWT(app, authenticate, identity)
 
 @app.before_first_request
 def create_tables():
@@ -18,6 +28,7 @@ def create_tables():
 api.add_resource(JobOrder, "/job_order/<string:id>")
 api.add_resource(JobOrderList, "/job_orders")
 api.add_resource(UUID, "/job_order/generate_uid")
+api.add_resource(UserRegister, "/register")
 
 if __name__ == "__main__":
     db.init_app(app)
