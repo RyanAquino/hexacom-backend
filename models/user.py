c8bb51d31134a1e177055d8e5aa8ac5dc4ba32d3
Heinrich Bangui <heinrichbangui@gmail.com>
2020-12-09 21:42:04 +0800

    HCM-23: Modify User Model and Resource to link with Job Orders

--- models/user.py
+++ models/user.py
@@ -6,15 +6,23 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
+    name = db.Column(db.String(80))
     password = db.Column(db.String(80))
 
-    def __init__(self, username, password):
+    job_orders = db.relationship("JobOrderModel", lazy="dynamic")
+
+    def __init__(self, username, name, password):
         self.username = username
+        self.name = name
         self.password = password
 
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+    def json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "username": self.username,
+            "job_orders": [job_order.json() for job_order in self.job_orders.all()],
+        }
 
     @classmethod
     def find_by_username(cls, username):
@@ -23,3 +31,11 @@ class UserModel(db.Model):
     @classmethod
     def find_by_id(cls, _id):
         return cls.query.filter_by(id=_id).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
