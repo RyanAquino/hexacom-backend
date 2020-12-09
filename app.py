da112b903f22be77e85056966cc50b3f8c596c9f
Heinrich Bangui <heinrichbangui@gmail.com>
2020-12-09 17:03:30 +0800

    HCM-19: Created Brand API and link to joborders

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from datetime import timedelta
 from security import authenticate, identity
 
 from resources.joborders import JobOrder, JobOrderList, UUID
+from resources.brands import Brand, BrandList
 from resources.user import UserRegister
 
 app = Flask(__name__)
@@ -16,7 +17,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # app.config['JWT_AUTH_URL_RULE'] = '/login' # IF we want to change authentication endpooint
 
 # Set to 8 hours, change the duration of the token
-app.config['JWT_EXPIRATION_DELTA'] = timedelta(hours=8)
+app.config["JWT_EXPIRATION_DELTA"] = timedelta(hours=8)
 
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
@@ -27,8 +28,10 @@ def create_tables():
     db.create_all()
 
 
-api.add_resource(JobOrder, "/job_order/<string:id>")
+api.add_resource(JobOrder, "/job_order/<string:_id>")
 api.add_resource(JobOrderList, "/job_orders")
+api.add_resource(Brand, "/brand/<string:name>")
+api.add_resource(BrandList, "/brands")
 api.add_resource(UUID, "/job_order/generate_uid")
 api.add_resource(UserRegister, "/register")
 
--- resources/user.py
+++ resources/user.py
@@ -4,15 +4,13 @@ from models.user import UserModel
 
 class UserRegister(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('username',
-                        type=str,
-                        required=True,
-                        help="This field can't be blank.")
+    parser.add_argument(
+        "username", type=str, required=True, help="This field can't be blank."
+    )
 
-    parser.add_argument('password',
-                        type=str,
-                        required=True,
-                        help="This field can't be blank.")
+    parser.add_argument(
+        "password", type=str, required=True, help="This field can't be blank."
+    )
 
     def post(self):
         data = UserRegister.parser.parse_args()
