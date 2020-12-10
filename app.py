f9c80a015f4a04d4f7fd06d9263c13a0008c92cb
Heinrich Bangui <heinrichbangui@gmail.com>
2020-12-10 11:25:13 +0800

    HCM-18: initial creation of swagger docs

--- app.py
+++ app.py
@@ -11,17 +11,35 @@ from resources.brands import Brand, BrandList
 from resources.user import UserRegister, UserList, User
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db" Change when no Docker
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db" #Change when no Docker
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:admin@db/hexacom"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # IF we want to change authentication endpooint
 app.config["JWT_AUTH_URL_RULE"] = "/login"
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
 
 # Set to 8 hours, change the duration of the token
 app.config["JWT_EXPIRATION_DELTA"] = timedelta(hours=8)
 
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
+
+from flask_swagger_ui import get_swaggerui_blueprint
+
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Hexacom-Python-Flask-REST"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+
 
 
 @app.before_first_request
